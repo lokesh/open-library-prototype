3803c5e44868d98d7e5197b3fe29f77156b44798
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2026-04-10 22:17:05 -0700

    Rename first import wizard step to Choose

--- src/components/ol-import-wizard.js
+++ src/components/ol-import-wizard.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css, svg } from 'lit';
 
-const STEP_LABELS = ['Service', 'Upload', 'Preview', 'Match', 'Confirm', 'Done'];
+const STEP_LABELS = ['Choose', 'Upload', 'Preview', 'Match', 'Confirm', 'Done'];
 
 export class OlImportWizard extends LitElement {
   static properties = {
